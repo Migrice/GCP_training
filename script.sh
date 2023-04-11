45f9d33cef6688e0495f47e0571e4dffe7aef596
migrice <efomenakuete@gmail.com>
2023-04-11 18:13:42 +0100

    Update script and add dockerfile

--- script.sh
+++ script.sh
@@ -2,10 +2,15 @@
 
 echo "Aspect Sentiment Triplet Extraction"
 
-pip install -r requirements.txt
+echo "Install dependancies"
+pip install torch==1.9.0+cu111 -f  https://download.pytorch.org/whl/cu111/torch_stable.html
+pip install transformers==4.8.2
 
 cd RoBMRC/
 
+echo "Begin Training"
 python ./tools/Main.py --mode train # For training
+
+echo "Begin Testing"
 python ./tools/Main.py --mode test # For testing
 
